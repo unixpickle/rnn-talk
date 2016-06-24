fafd559f15a3a705f8a675c0c4fa1913663fc1e0
Alex Nichol <unixpickle@gmail.com>
2016-06-24 17:23:50 -0400

    drafted talk sub-command

--- talk.go
+++ talk.go
@@ -1,7 +1,46 @@
 package main
 
-import "errors"
+import (
+	"io/ioutil"
+
+	"github.com/unixpickle/num-analysis/linalg"
+	"github.com/unixpickle/wav"
+	"github.com/unixpickle/weakai/rnn"
+)
 
 func Talk(rnnFile, outputFile string, seconds float64) error {
-	return errors.New("not yet implemented.")
+	talkerData, err := ioutil.ReadFile(rnnFile)
+	if err != nil {
+		return err
+	}
+	talker, err := DeserializeTalker(talkerData)
+	if err != nil {
+		return err
+	}
+
+	chunkSize, _ := talker.Compressor.Dims()
+	count := int(float64(talker.Channels*talker.SampleRate) * seconds / float64(chunkSize))
+
+	var output []wav.Sample
+
+	runner := &rnn.Runner{Block: talker.Block}
+	lastOutput := make(linalg.Vector, talker.Block.StateSize())
+	tempSamples := make([]wav.Sample, chunkSize)
+	for i := 0; i < count; i++ {
+		lastOutput = runner.StepTime(lastOutput)
+		mat := &linalg.Matrix{
+			Rows: 1,
+			Cols: len(lastOutput),
+			Data: lastOutput,
+		}
+		decompressed := talker.Compressor.Decompress(mat)
+		for i, x := range decompressed.Data {
+			tempSamples[i] = wav.Sample(x)
+		}
+		output = append(output, tempSamples...)
+	}
+
+	outSound := wav.NewPCM8Sound(talker.Channels, talker.SampleRate)
+	outSound.SetSamples(output)
+	return wav.WriteFile(outSound, outputFile)
 }
